f55ca364382fafa77ca9a7dfe0e970e27bfd20f1
Anders Birkenes <datagutten@datagutten.net>
2025-12-15 17:06:56 +0100

    Add compose file path to bash script

--- backfill_people_count.sh
+++ backfill_people_count.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e
-docker compose run --rm parse_data
-docker compose run --rm people_count_openmetrics
-docker compose exec prometheus find /prometheus/people_count -name "metrics_*.txt" -exec promtool tsdb create-blocks-from openmetrics {} /prometheus \;
+SCRIPT_DIRECTORY="$(dirname "$BASH_SOURCE")"
+
+docker compose -f ${SCRIPT_DIRECTORY}/docker-compose.yml run --rm parse_data
+docker compose -f ${SCRIPT_DIRECTORY}/docker-compose.yml run --rm people_count_openmetrics
+docker compose -f ${SCRIPT_DIRECTORY}/docker-compose.yml exec prometheus find /prometheus/people_count -name "metrics_*.txt" -exec promtool tsdb create-blocks-from openmetrics {} /prometheus \;
